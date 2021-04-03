1cf866773c36844e5acf86241551c7e4041a5255
Revin Tanri <tanrirevin@gmail.com>
2021-04-03 10:54:59 +0800

    Work and Note on OOP

--- GA/Labs/w03d3/OOP_lab.js
+++ GA/Labs/w03d3/OOP_lab.js
@@ -1,4 +1,5 @@
 // Q1
+// 'Pet' is the parents
 class Pet {
     owner = ""
     name = ""
@@ -6,85 +7,134 @@ class Pet {
         console.log('walka walka')
     }
 
-    constructor(owner, name) {
-        this.owner = owner
+    constructor(name) {
         this.name = name
     }
 }
 
-let Pompom = new Pet("Jon", "Fayete");
-console.log(Pompom)
-Pompom.walk();
+// use the small words in new object 'pompom'
+let pompom = new Pet("Fayete")
+console.log("--=== testing pompom with Pet class ===--")
+console.log(pompom)
+pompom.walk();
 
 
-// Q2.
+// Q2. with new attributes PRICE
 
+// 'Dog' is the child of 'Pet'
 class Dog extends Pet {
     price = 20
+
+    constructor(name, price) {
+        // to pull the constructor from parents
+        super(name)
+        this.price = price;
+    }
+
     bark() {
         console.log("Bark")
     }
     chaseTail() {
-        console.log("oh boy oh boy")
+        console.log("keep spining")
     }
+
+    // getter 
     getPrice() {
-        return this.price;
+        return this.price
+    }
+
+    // setter - if object want to change the price like Question 4
+    setPrice(price) {
+        this.price = price
     }
 }
 
 // create new object
-let Bulldog = new Dog("Ms Susy", "Handsome Boy");
-Bulldog.bark();
-Bulldog.getPrice();
+let bulldog = new Dog("Handsome Boy", 40);
+console.log("--=== testing bulldog with Dog class ===--")
+bulldog.bark();
+bulldog.chaseTail();
+console.log(bulldog.getPrice());
+
 
 
 // Q3 . Cat Class
+
+// 'Cat' is the child of 'Pet'
 class Cat extends Pet {
     price = 10
+
+    constructor(name, price) {
+        // to pull the constructor from parents
+        super(name)
+        this.price = price
+    }
+
     purr() {
         console.log("purrrr")
     }
     clean() {
-        console.log("cleaning")
+        console.log("cleaning at day and night")
     }
     getPrice() {
         return this.price;
     }
+
+    // method over-riding
     walk() {
-        console.log("strut strut")
+        console.log("strut strut - change from walka walka")
     }
 }
 
 // create new cat object
-let Jessy = new Cat("Ms Monica", "Jessy");
-Jessy.clean();
-Jessy.walk();
-Jessy.getPrice();
-Jessy.purr();
+let jessy = new Cat("Jessy", 90);
+console.log("--=== testing jessy with Cat class ===--")
+jessy.clean();
+jessy.walk();
+console.log(jessy.getPrice());
+jessy.purr();
 
 
 // 4. update one property of the dog after it has been created and log it to check
-Dog.bark = () => {
-    console.log("Woof woof")
-};
-Dog.price = 35; //price go up because of COVID
+// change the Dog.chaseTail()
+bulldog.name = "Hercules"
+
+// instead of doing this, in JS dnot have called public/private/protected atribute in OOP
+// bulldog.price = 100;
+
+// change with SETTER
+bulldog.setPrice(100);
 
 // testing
-Dog.bark();
-Bulldog.bark(); // ?? Why Bulldog.bark() still the old value "bark"
-console.log(Bulldog.getPrice()); // still old price '20', not the updated '35'
-
-
-console.log(typeof Dog.bark); // function
-console.log(typeof Dog.getPrice); // why 'undefined', not 'function' like Dog.bark()
-console.log(typeof Bulldog.getPrice()); //number
+console.log("--=== testing bulldog after change chaseTail() in Dog class ===--")
+console.log(bulldog.name);
+bulldog.chaseTail(); // ?? Why Bulldog.bark() still the old value "bark"
+console.log(bulldog.getPrice()); // still old price '20', not the updated '35'
 
 
 
+// 5. Updated one property of the cat after it has been created and log it to check
+console.log("--=== testing jessy after change clean() in Dog class ===--")
+jessy.clean();
+
+// you cannot chang ethe CLASS OOP directly
+// but use GETTER and SETTER
 
 
-// 5. Updated one property of the cat after it has been creatd and log it to check
-Jessy.price = 55;
-console.log(Jessy.price);
-console.log(Jessy.getPrice()); // give '55', why Bulldog.getPrice() still old value
-Jessy.clean();
+
+/* ============ Streches  ============ */
+
+// Using classes, generate a deck of cards, stored in an array
+// - cards are objects and should have a value, a face, a suit, a boolean of whether they are faceUp etc.
+// - there are 13 cards per suit, values should match what they are in BlackJack (or another game), - Ace, by default is equal to 11, - cards 2-10 share the same face and value - Jack, Queen and King have a value of 10
+
+// Figure out how to shuffle the array and console.log the top card (Hint: google it)
+
+// Figure out how to compare the top and bottom card and console.log both cards and a message that says which card is bigger (or a tie)
+
+// class Card {
+//     value: ""
+//     face: ""
+//     suit: ""
+//     faceUp: true
+// }
--- GA/Note/Week-3-1.js
+++ GA/Note/Week-3-1.js
@@ -59,76 +59,153 @@
 // highly effective algorithm, divides problems by half each time
 
 
+// /* ============================================ */
+// /* ======= Object Oriented Programming ======== */
+// /* ============================================ */
+
+
+// // 1. Using 'class' as a blueprint for a type of object
+// // new obj created from a class will have all the attributes and functions defined in class
+// // start with Capital Letter: class Person {...}
+
+// // blueprint, cannot be used directly
+// class Person {
+//     name = "John"
+//     age = 12
+//     gender = "m"
+//     currentHealth = {}
+
+//     // constructor : allow class to accept parameter, to create object with diff attributes
+//     // will accept paramaters and passed to attributes
+//     constructor(name, age, gender) {
+//         // this 'THIS.name' will set the new objects' name (e.g. Jon's name)
+//         this.name = name
+//         this.age = age
+//         this.gender = gender
+//     }
+
+//     speak(words) { //add attribute
+//         console.log(words)
+//     }
+// }
+
+
+// let jon = new Person("Jonathan", 25, "m");
+// jon.speak("mcspicy")
+// console.log(jon.age)
+
+// let minshan = new Person("Min Shan", 28, "m");
+// minshan.speak("Fille-o-fish")
+// console.log(minshan.age);
+
+
+// class Car {
+//     brand = ""
+//     name = ""
+//     tankCapacity = 0
+//     currentPetrolLevel = 0
+
+//     constructor(brand, name, tankCapacity) {
+//         this.brand = brand
+//         this.name = name
+//         this.tankCapacity = tankCapacity
+//     }
+
+//     greetOwnerWhenIgnited() {
+//         console.log("Hi There")
+//     }
+
+//     pumpPetrol(volOfPetrolToPump) {
+//         let newPetrolLevel = this.currentPetrolLevel + volOfPetrolToPump
+
+//         if (newPetrolLevel > this.tankCapacity) {
+//             return false
+//         }
+
+//         this.currentPetrolLevel = newPetrolLevel
+//         return true
+//     }
+// }
+
+// let CurrentCar = new Car("Toyota", "Prius", 30);
+// CurrentCar.pumpPetrol(45);
+// CurrentCar.greetOwnerWhenIgnited();
+// // CurrentCar.pumpPetrol(15);
+// console.log(CurrentCar);
+
 /* ============================================ */
-/* ======= Object Oriented Programming ======== */
+/* ======= OOP Concept ======== */
 /* ============================================ */
 
+// 1. Abstraction
 
-// 1. Using 'class' as a blueprint for a type of object
-// new obj created from a class will have all the attributes and functions defined in class
-// start with Capital Letter: class Person {...}
+// 2. Encapsulation
 
-// blueprint, cannot be used directly
+// 3. Polymorphism
+
+// 4. Inheritance
+// class can have child classes
+
 class Person {
-    name = "John"
-    age = 12
-    gender = "m"
-    currentHealth = {}
-
-    // constructor : allow class to accept parameter, to create object with diff attributes
-    // will accept paramaters and passed to attributes
     constructor(name, age, gender) {
-        // this 'THIS.name' will set the new objects' name (e.g. Jon's name)
         this.name = name
         this.age = age
         this.gender = gender
     }
 
-    speak(words) { //add attribute
+    speak(words) {
         console.log(words)
     }
-}
 
-
-let jon = new Person("Jonathan", 25, "m");
-jon.speak("mcspicy")
-console.log(jon.age)
-
-let minshan = new Person("Min Shan", 28, "m");
-minshan.speak("Fille-o-fish")
-console.log(minshan.age);
-
-
-class Car {
-    brand = ""
-    name = ""
-    tankCapacity = 0
-    currentPetrolLevel = 0
-
-    constructor(brand, name, tankCapacity) {
-        this.brand = brand
-        this.name = name
-        this.tankCapacity = tankCapacity
-    }
-
-    greetOwnerWhenIgnited() {
-        console.log("Hi There")
-    }
-
-    pumpPetrol(volOfPetrolToPump) {
-        let newPetrolLevel = this.currentPetrolLevel + volOfPetrolToPump
-
-        if (newPetrolLevel > this.tankCapacity) {
-            return false
-        }
-
-        this.currentPetrolLevel = newPetrolLevel
-        return true
+    greet() {
+        console.log('Hi! My name is ' + this.name)
     }
 }
 
-let CurrentCar = new Car("Toyota", "Prius", 30);
-CurrentCar.pumpPetrol(45);
-CurrentCar.greetOwnerWhenIgnited();
-// CurrentCar.pumpPetrol(15);
-console.log(CurrentCar);
+
+class Superman extends Person {
+    heroAttire = ""
+
+    constructor(name, age, gender, attire) {
+        // use the functionality from parents class
+        // executing parent's class's constructor
+        // Person(name, age, gender)
+        super(name, age, gender)
+
+        this.heroAttire = attire
+    }
+
+    fly() {
+        console.log('Up up and away!');
+    }
+
+    // method over-riding
+    greet() {
+        console.log("Look at me fly peasants")
+    }
+}
+
+
+let superman = new Superman('Tony Stark', 99, 'm')
+
+// Superman can do all things a normal Person can do
+superman.speak("Good Morning America");
+superman.greet();
+
+// Superman can do his own unique ability
+superman.fly()
+
+
+// Superman can do things differently than a normal Person
+// method over-riding
+
+// We can use JS keyword super in a child class to reference attributes and functionalities of parent class
+
+// Superman has his own unique attributes
+console.log(superman.name);
+console.log(superman.age);
+console.log(superman.gender);
+console.log(superman.heroAttire);
+
+// child class will auto-use parent's contructor
+// if you want to use new attributes/additional parameters, you need to use SUPER to pull the parents constructor to child
