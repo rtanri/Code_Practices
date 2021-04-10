d1bbfc7680394518ea2d622aa372baaf4f22c997
Revin Tanri <tanrirevin@gmail.com>
2021-04-10 14:31:17 +0800

    part 2 - Poke API: Setup the bootstrap and AJAX API

--- GA/Labs/w05d1/pokeapi/main.js
+++ GA/Labs/w05d1/pokeapi/main.js
@@ -1,24 +1,60 @@
 jQuery(document).ready(function () {
     console.log("Pokeapi")
 
+    // 5. Work on Submit button function
     let searchSubmitButton = $('#search-form-btn')
     // when you click this submit, page being reloaded
-    searchSubmitButton.on('click', function () {
-        console.log("Form Submitted")
+    searchSubmitButton.on('click', function (event) {
+        event.preventDefault(); // console.log("Form Submitted") - to test
+
+        // grab the value of form input id=pokemon_name
+        let pokemonName = $('#pokemon_name').val()
+        pokemonName = pokemonName.trim() // remove  whitespace
+
+        if (pokemonName.length === 0) {
+            alert('Please fill in your fav pokemon')
+            return
+        }
+
+        // 1. Make the AJAX
+        // 6. Copy paste the AJAX inside here
+        // we use 2nd method: jQuery.ajax( [settings ] ) ===== from https://api.jquery.com/jquery.ajax/
+        $.ajax({
+            // 7. Update the url with + pokemonName
+            url: "https://pokeapi.co/api/v2/pokemon/" + pokemonName,
+            method: "GET",
+            // see if this API call success or not
+            success: function (response) {
+                let searchResultDiv = $('#search-result')
+
+                // modify the img source with TEMPLATE LITERAL with back-tick `
+                searchResultDiv.html(`
+                <div class="card">
+                    <img src="${response.sprites.front_default}" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">${response.name} ${response.id}</h5>
+                        <p class="card-text">${response.base_experience}</p>
+                </div>
+                </div>
+                `)
+                // console.log(response.id)
+                // console.log(response.name)
+                // console.log(response.base_experience)
+                // console.log(response.sprites.front_default)
+            },
+            // function to show when it error
+            error: function (err) {
+                let searchMsgElement = $('#search-msg')
+                searchMsgElement.css("display", "block")
+
+                if (err.status === 404) {
+                    searchMsgElement.children(".alert").text("Pokemon is not found, try again")
+                } else {
+                    searchMsgElement.children(".alert").text("Unknown Error")
+                }
+            }
+        })
     })
 
-    // // we use 2nd method: jQuery.ajax( [settings ] ) ===== from https://api.jquery.com/jquery.ajax/
-    // $.ajax({
-    //     // where i want to post my url to
-    //     url: "https://pokeapi.co/api/v2/pokemon/charmander",
-    //     method: "GET",
-    //     // see if this API call success or not
-    //     success: function (response) {
-    //         console.log(response.base_experience)
-    //     },
-    //     // function to show when it error
-    //     error: function (err) {
-    //         console.log(err)
-    //     }
-    // })
+
 })
