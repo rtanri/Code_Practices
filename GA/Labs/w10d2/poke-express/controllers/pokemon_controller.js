6daa31bba2efdbaf73cbb7f6a45ba17dc85ea59a
Revin Tanri <tanrirevin@gmail.com>
2021-05-18 19:43:44 +0800

    Add few error posibilities in pokemon_controller

--- GA/Labs/w10d2/poke-express/controllers/pokemon_controller.js
+++ GA/Labs/w10d2/poke-express/controllers/pokemon_controller.js
@@ -1,34 +1,63 @@
 const {
         pokemon,
         Pokemon
-} = require('../models/pokemon')
+} = require('../models/pokemon');
+const _ = require('lodash')
 
 module.exports = {
+
         index: (req, res) => {
                 res.render('index', {
                         pokemon
-                })
+                });
         },
 
         newPokemonForm: (req, res) => {
-                res.render('create')
+                res.render('create');
         },
+
         show: (req, res) => {
-                const selectedPokemon = pokemon[Number(req.params.id)]
-                console.log(selectedPokemon)
+                let id = Number(req.params.id)
+
+                // check if id is of a number type
+                if (isNaN(id)) {
+                        res.statusCode = 400
+                        res.send('id must be a valid number')
+                        return
+                }
+
+                // check that given id is within expected range
+                // undefined, null, 0, '', {}, []
+                // these are all considered "empty"
+                if (_.isEmpty(pokemon[id])) {
+                        res.statusCode = 404
+                        res.send('cannot find pokemon with the given id')
+                        return
+                }
 
                 res.render('show', {
-                        pokemon: selectedPokemon,
-                        nextPokemonIndex: Number(req.params.id) + 1,
-                        beforePokemonIndex: Number(req.params.id) - 1
-                })
+                        pokemon: pokemon[id]
+                });
         },
+
         create: (req, res) => {
-                const newPokemon = new Pokemon(req.body.name, req.body.img, pokemon.length)
+                let givenName = req.body.name.toLowerCase()
+
+                // check if given pokemon already exist
+                let exist = pokemon.find(element => {
+                        return element.name === givenName
+                });
+                if (exist) {
+                        res.statusCode = 409
+                        res.send('pokemon with given name already exists')
+                        return
+                }
+
+                const newPokemon = new Pokemon(pokemon.length, givenName, req.body.img);
                 pokemon.push(newPokemon);
                 res.render('created', {
                         pokemon: newPokemon
-                })
+                });
         }
 
 }
